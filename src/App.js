4b66b6c40368541b2056fadcefbd1ab0e0518f19
Austin Kabiru <austin.kabiru@andela.com>
2017-08-28 19:21:43 +0300

    😏

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-function createStore(reducer) {
-  let state = 0;
+function createStore(initialState, reducer) {
+  let state = initialState;
 
   const getState = () => (state);
 
@@ -12,3 +12,63 @@ function createStore(reducer) {
     dispatch,
   };
 }
+
+// reducers must be pure functions
+function messageReducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    return {
+      messages: state.messages.concat(action.message),
+    };
+  } else if (action.type === 'DELETE_MESSAGE') {
+    // Immutable deletion strategy
+    // all elements from 0 to action index
+    // all elements from action index + 1 to the end of the array
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice(action.index + 1, state.messages.length)
+      ]
+    }
+  } else {
+    return state;
+  }
+}
+
+
+// App
+
+const initialState = {
+  messages: []
+};
+
+const store = createStore(initialState, messageReducer);
+
+const addMessageAction1 = {
+  type: 'ADD_MESSAGE',
+  message: 'How does it look, Neil?',
+};
+
+store.dispatch(addMessageAction1);
+const stateV1 = store.getState();
+
+const addMessageAction2 = {
+  type: 'ADD_MESSAGE',
+  message: 'Looking good.',
+};
+
+store.dispatch(addMessageAction2);
+const stateV2 = store.getState();
+
+const deleteMessageAction = {
+  type: 'DELETE_MESSAGE',
+  index: 0,
+};
+
+store.dispatch(deleteMessageAction);
+const stateV3 = store.getState();
+
+
+
+console.log('State v1:', stateV1);
+console.log('State v2:', stateV2);
+console.log('State v3:', stateV3);
