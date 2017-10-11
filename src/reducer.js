aec9bd47ccdb2acb9c943d0cc67c813a545fe9a3
Austin Kabiru <austin.kabiru@andela.com>
2017-10-11 18:51:51 +0300

    refactor: reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,20 @@
 const reducer = function (state, action) {
-  if (action.type === 'ADD_MESSAGE') {
-    return {
-      messages: state.messages.concat(action.message),
-    };
-  } else if (action.type === 'DELETE_MESSAGE') {
-    return {
-      messages: [
-        ...state.messages.slice(0, action.index),
-        ...state.messages.slice(
-          action.index + 1, state.messages.length
-        ),
-      ],
-    };
-  } else {
-    return state;
+  switch(action.type) {
+    case 'ADD_MESSAGE':
+      return {
+        messages: state.messages.concat(action.message)
+      }
+    case 'DELETE_MESSAGE':
+      return {
+        messages: [
+          ...state.messages.splice(0, action.index),
+          ...state.messages.slice(
+            action.index + 1, state.messages.length
+          )
+        ]
+      }
+    default:
+      return state
   }
 }
 
